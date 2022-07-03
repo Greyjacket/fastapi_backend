0140809c41264c807163a6c6afb3e1dd1aa011d7
Greyjacket <asch716@gmail.com>
2022-07-03 01:05:33 -0400

    expanded contracts

--- agents/schemas.py
+++ agents/schemas.py
@@ -1,11 +1,13 @@
 from pydantic import BaseModel
-
+from contracts import schemas
+ 
 
 class Agent(BaseModel):
     id: int
     name: str
     phone: str
     organization: str
-    
+    contracts: list[schemas.Contract] = []
+
     class Config:
         orm_mode = True
